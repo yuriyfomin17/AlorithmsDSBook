7ff760789d418db25344a35a074401dd99bd8d53
yuriyfomin17 <fominyura2012@gmail.com>
2021-10-06 11:52:18 +0300

    learning

--- src/Chapter1Section2/ChapterExercises/Exe211SmartDate.java
+++ src/Chapter1Section2/ChapterExercises/Exe211SmartDate.java
@@ -4,6 +4,26 @@ public class Exe211SmartDate {
     private final int day;
     private final int month;
     private final int year;
+    private static final int[] centuryCode = { 4, 2, 0, 6}; // 1700, 1800, 1900, 2000 respectively
+    private static final int[] monthsCode = {1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6}; // Jan, Feb, Mar, Apr, May, June, July, Aug, Sep, Oct, Nov, Dec
+    public enum WeekDays{
+        // Caution order is important. Ordinal of Enum is equal to following codes  0, 1, 2, 3, 4, 5, 6
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        UNKNOWN;
+        public static WeekDays getById(int id) {
+            for (WeekDays dayOfWeek : WeekDays.values()){
+                if (dayOfWeek.ordinal() + 1 == id) return dayOfWeek;
+            }
+            return UNKNOWN;
+        }
+
+    }
 
     public Exe211SmartDate(int aDay, int aMonth, int aYear){
         if (!isValidDate(aDay, aMonth, aYear)) throw new IllegalArgumentException("Not correct date");
@@ -50,12 +70,39 @@ public class Exe211SmartDate {
                 '}';
     }
 
-    public static void main(String[] args) {
-        for (int i = 2020; i <= 2050 ; i++) {
-            if (isItLeapYear(i)) System.out.printf("%d is leap year\n", i);
-            else System.out.printf("%d is not leap year\n", i);
+    public static WeekDays getDayOfWeek(Exe211SmartDate exe211SmartDate){
+        int aDay = exe211SmartDate.getDay();
+        int aMonth = exe211SmartDate.getMonth();
+        int aYear = exe211SmartDate.getYear();
+
+        // The key value method
+        int lastTwoDigitOfYear = Integer.parseInt(Integer.toString(aYear).substring(2, 4));
+        int runningTotal  = lastTwoDigitOfYear / 4 + aDay + monthsCode[aMonth - 1];
+        if (isItLeapYear(aYear)) runningTotal -= 1;
+        int currCentury = getCentury(aYear);
+        int currYear = aYear;
+        while (currCentury != 17 && currCentury != 18 && currCentury != 19 && currCentury != 20){
+            currYear -= 400;
+            currCentury = getCentury(currYear);
         }
-        Exe211SmartDate smartDate1 = new Exe211SmartDate(4, 18, 1989);
-        Exe211SmartDate invalidDate = new Exe211SmartDate(2, 30, 2016);
+        if (currCentury == 17) runningTotal += centuryCode[0];
+        if (currCentury == 18) runningTotal += centuryCode[1];
+        if (currCentury == 19) runningTotal += centuryCode[2];
+        if (currCentury == 20) runningTotal += centuryCode[3];
+
+        int dayRemainder = (runningTotal + lastTwoDigitOfYear)% 7;
+        return WeekDays.getById(dayRemainder);
+    }
+    public static int getCentury(int aYear){
+        return Integer.parseInt(Integer.toString(aYear).substring(0, 2));
+    }
+
+    public static void main(String[] args) {
+//        for (int i = 2020; i <= 2050 ; i++) {
+//            if (isItLeapYear(i)) System.out.printf("%d is leap year\n", i);
+//            else System.out.printf("%d is not leap year\n", i);
+//        }
+        Exe211SmartDate smartDate1 = new Exe211SmartDate(12, 4, 2030);
+        System.out.println(getDayOfWeek(smartDate1));
     }
 }
