da40fd27d7fc218902fdbfb0bf3a4f6c152aadbe
yuriyfomin17 <fominyura2012@gmail.com>
2021-11-09 22:27:32 +0200

    efficiency improved

--- src/Chapter1Section4/Exercises/Exe1410BinarySearchSmallestIndex.java
+++ src/Chapter1Section4/Exercises/Exe1410BinarySearchSmallestIndex.java
@@ -1,44 +1,42 @@
 package Chapter1Section4.Exercises;
 
 public class Exe1410BinarySearchSmallestIndex {
-    static public int binarySearch(int key, int[] arr){
-        int low = 0;
+
+
+    static public int binarySearchBiggestIndex(int key, int[] arr){
         int high = arr.length - 1;
-        while (low <= high){
+        int low  = 0;
+        while (low < high){
             int mid = low + (high - low) / 2;
-            if (key > arr[mid]) low = mid + 1;
-            else if (key < arr[mid]) high = mid - 1;
-            else return binarySearchLowestIndex(arr, mid);
+            if (arr[mid] > key) high = mid - 1;
+            else if (arr[mid] <= key)  low = mid + 1;
         }
+        if (arr[low] == key) return low;
+        else if (arr[low - 1] == key) return low - 1;
         return -1;
     }
-    static public int binarySearchLowestIndex(int[] arr, int midIndex){
+    static public int binarySearchLowestIndex(int key, int[] arr){
+        int high = arr.length - 1;
         int low = 0;
-        while (low < midIndex){
-            int mid = low + (midIndex - low) / 2;
-            if ( arr[mid] < arr[midIndex]) low = mid;
-            else return mid;
+        while (low < high){
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= key) high = mid - 1;
+            else if (arr[mid] < key) low = mid + 1;
         }
-        return midIndex;
-    }
-    static public int binarySearchLowestIndexFaster(int[] arr, int midIndex, int currentLow){
-        int low = currentLow;
-        while (low < midIndex){
-            int mid = low + (midIndex - low) / 2;
-            if ( arr[mid] < arr[midIndex]  ) low = mid;
-            else return mid;
-        }
-        return midIndex;
+        if (arr[high] == key) return high;
+        else if (arr[high + 1] == key)return high + 1;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[] testArray1 = {3, 4, 4, 4,  5, 6, 10, 15, 20, 20, 20, 20, 21};
+        int[] testArray1 = {3, 4, 4, 4, 4, 5, 6, 6, 6, 6, 10, 15, 20, 20, 20, 20, 21};
         int elementToSearch1 = 4;
         int elementToSearch2 = 20;
-        int elementToSearch3 = -5;
-        System.out.println("Binary Search: " + binarySearch(elementToSearch1, testArray1) + " Expected: 1");
-        System.out.println("Binary Search: " + binarySearch(elementToSearch2, testArray1) + " Expected: 8");
-        System.out.println("Binary Search: " + binarySearch(elementToSearch3, testArray1) + " Expected: -1");
+        System.out.println("Binary Search Lowest Index: " + binarySearchLowestIndex( elementToSearch1, testArray1) + " Expected: 1");
+        System.out.println("Binary Search Biggest Index: " + binarySearchBiggestIndex( elementToSearch1, testArray1) + " Expected: 4");
+
+        System.out.println("Binary Search Lowest Index: " + binarySearchLowestIndex( elementToSearch2, testArray1) + " Expected: 12");
+        System.out.println("Binary Search Biggest Index: " + binarySearchBiggestIndex(elementToSearch2, testArray1) + " Expected: 15" );
     }
 
 }
