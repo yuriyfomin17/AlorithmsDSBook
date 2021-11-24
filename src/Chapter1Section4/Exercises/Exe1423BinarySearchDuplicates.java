d2a751024402ef699c18a5b29070151b2cc222f0
yuriyfomin17 <fominyura2012@gmail.com>
2021-11-24 15:39:57 +0200

    notes

--- src/Chapter1Section4/Exercises/Exe1423BinarySearchDuplicates.java
+++ src/Chapter1Section4/Exercises/Exe1423BinarySearchDuplicates.java
@@ -16,20 +16,56 @@ public class Exe1423BinarySearchDuplicates {
         StdOut.println("First index of element -8: " + firstRankElement2 + " Expected: -1");
         StdOut.println("First index of element 5: " + firstRankElement3 + " Expected: 11");
         StdOut.println("First index of element 2: " + firstRankElement4 + " Expected: 3");
+        System.out.println();
+        StdOut.println("First index of element -4: " + binarySearchFirstIndexRecursionFasterFirstIndex(array, 0, array.length - 1, -4) + " Expected: 0");
+        StdOut.println("First index of element -8: " + binarySearchFirstIndexRecursionFasterFirstIndex(array, 0, array.length - 1, -8) + " Expected: -1");
+        StdOut.println("First index of element 5: " + binarySearchFirstIndexRecursionFasterFirstIndex(array, 0, array.length - 1, 5) + " Expected: 11");
+        StdOut.println("First index of element 2: " + binarySearchFirstIndexRecursionFasterFirstIndex(array, 0, array.length - 1, 2) + " Expected: 3");
+        System.out.println();
+        StdOut.println("First index of element -4: " + binarySearchIndexRecursionFasterLastIndex(array, 0, array.length - 1, -4) + " Expected: 0");
+        StdOut.println("First index of element -8: " + binarySearchIndexRecursionFasterLastIndex(array, 0, array.length - 1, -8) + " Expected: -1");
+        StdOut.println("First index of element 5: "  + binarySearchIndexRecursionFasterLastIndex(array, 0, array.length - 1, 5) + " Expected: 11");
+        StdOut.println("First index of element 2: "  + binarySearchIndexRecursionFasterLastIndex(array, 0, array.length - 1, 2) + " Expected: 9");
+
     }
+
     // It is O(log N) however, if there would be a large number of same numbers then it
     // would be O(K) where K iterations which is equal to frequency of numbers which are same and equal to our target
-    public static int binarySearchFirstIndex(int[] arr, int target){
+    public static int binarySearchFirstIndex(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
-        while (low <= high){
+        while (low <= high) {
             int mid = low + (high - low) / 2;
             int prevNum = mid - 1 >= 0 ? arr[mid - 1] : -Integer.MAX_VALUE;
 //            System.out.println("prevNum:" + prevNum);
             if (target > arr[mid]) low = mid + 1;
-            else if (target < arr[mid] || prevNum == target ) high = mid - 1;
+            else if (target < arr[mid] || prevNum == target) high = mid - 1;
             else return mid;
         }
         return -1;
+    }
+
+    public static int binarySearchFirstIndexRecursionFasterFirstIndex(int[] arr, int low, int high, int target) {
+        if (low > high) return -1;
+        int mid = low + (high - low) / 2;
+        if (target > arr[mid]) return binarySearchFirstIndexRecursionFasterFirstIndex(arr, mid + 1, high, target);
+        else if (target < arr[mid]) return binarySearchFirstIndexRecursionFasterFirstIndex(arr, low, mid - 1, target);
+        else {
+            int lowestIndex = binarySearchFirstIndexRecursionFasterFirstIndex(arr, low, mid - 1, target);
+            if (lowestIndex != -1) return lowestIndex;
+            else return mid;
+        }
+    }
+
+    public static int binarySearchIndexRecursionFasterLastIndex(int[] arr, int low, int high, int target) {
+        if (low > high) return -1;
+        int mid = low + (high - low) / 2;
+        if (target > arr[mid]) return binarySearchIndexRecursionFasterLastIndex(arr, mid + 1, high, target);
+        else if (target < arr[mid]) return binarySearchIndexRecursionFasterLastIndex(arr, low, mid - 1, target);
+        else {
+            int highestIndex = binarySearchIndexRecursionFasterLastIndex(arr, mid + 1, high, target);
+            if (highestIndex != -1) return highestIndex;
+            else return mid;
+        }
     }
 }
