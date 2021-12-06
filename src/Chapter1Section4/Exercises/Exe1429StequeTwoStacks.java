c5e53d2173498542dfe3cd900324f72b56aa875c
yuriyfomin17 <fominyura2012@gmail.com>
2021-12-06 15:45:51 +0200

    notes

--- src/Chapter1Section4/Exercises/Exe1429StequeTwoStacks.java
+++ src/Chapter1Section4/Exercises/Exe1429StequeTwoStacks.java
@@ -1,12 +1,13 @@
 package Chapter1Section4.Exercises;
 
+import Chapter1Section3.ChapterExercises.Exe1332Steque;
+
 import java.util.Iterator;
 
 public class Exe1429StequeTwoStacks {
 
     public static void main(String[] args) {
-
-
+        checkStequeTwoStack2();
     }
 
     public static void checkSteque() {
@@ -19,6 +20,41 @@ public class Exe1429StequeTwoStacks {
         System.out.println(steque + " expected: 4 3 2 1 5");
         steque.dequeue();
         System.out.println(steque + " expected: 3 2 1 5");
+    }
+
+    public static void checkStequeTwoStack(){
+        StequeWithStack steque = new StequeWithStack();
+        steque.push(1);
+        steque.push(2);
+        steque.push(3); // 3 2 1
+        steque.pop(); // 2 1
+        steque.enqueue(5);
+        steque.enqueue(6); // 6 5 1 2
+        System.out.println("Steque items: " + steque);
+        System.out.println("Expected: 6 5 1 2");
+
+        steque.push(4); // 4 2 1 6 5
+
+        System.out.println("Steque items: " + steque);
+        System.out.println("Expected: 4 2 1 5 6");
+    }
+
+    public static void checkStequeTwoStack2(){
+        StequeWithStack steque = new StequeWithStack();
+        steque.push(1);
+        steque.push(2);
+        steque.push(3);
+        System.out.println(steque.pop());
+        System.out.println(steque.pop());
+        steque.enqueue(4);
+        steque.enqueue(5);
+        System.out.println("Size: " + steque.size() + " expected: 3");
+        System.out.println("isEmpty: " + steque.isEmpty() + " expected: false");
+        System.out.println(steque.pop());
+        System.out.println(steque.pop());
+        System.out.println("Expected output from pop:3 2 1 4");
+
+
     }
 
     static class Steque<Item> {
@@ -99,6 +135,93 @@ public class Exe1429StequeTwoStacks {
             this.item = item;
             this.next = null;
 
+        }
+    }
+
+    static class Stack<Item> {
+        Node first = null;
+        int N = 0;
+
+        public void push(Item item) {
+            Node newFirst = new Node(item);
+            if (!isEmpty()) newFirst.next = this.first;
+            this.first = newFirst;
+            N++;
+        }
+
+        public Item pop() {
+            Item item = (Item) this.first.item;
+            this.first = this.first.next;
+            N--;
+            return item;
+        }
+
+        public boolean isEmpty() {
+            return N == 0;
+        }
+
+        public int size() {
+            return N;
+        }
+
+        public String toString() {
+            String result = "";
+            Stack.StackIterator stackIterator = new Stack.StackIterator();
+            while (stackIterator.hasNext()) result += " " + stackIterator.next();
+            return result;
+
+        }
+
+        public class StackIterator implements Iterator<Item> {
+            Node current = first;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Item next() {
+                Item item = (Item) current.item;
+                current = current.next;
+                return item;
+            }
+        }
+    }
+
+    static class StequeWithStack<Item> {
+        Stack topStack = new Stack();
+        Stack bottomStack = new Stack();
+
+        // push item to the bottom of the stack
+        public void enqueue(Item item) {
+            if (bottomStack.isEmpty()) while (!topStack.isEmpty()) bottomStack.push(topStack.pop());
+            bottomStack.push(item);
+        }
+
+        // push item to the top of stack
+        public void push(Item item) {
+            if (topStack.isEmpty()) while (!bottomStack.isEmpty()) topStack.push(bottomStack.pop());
+            topStack.push(item);
+        }
+
+        // remove top item from the stack
+        public Item pop() {
+            if (topStack.isEmpty()) while (!bottomStack.isEmpty()) topStack.push(bottomStack.pop());
+
+            return (Item) topStack.pop();
+        }
+
+        public boolean isEmpty() {
+            return topStack.isEmpty() && bottomStack.isEmpty();
+        }
+
+        public int size() {
+            return topStack.size() + bottomStack.size();
+        }
+
+        public String toString() {
+            return topStack.toString() + bottomStack.toString();
         }
     }
 }
